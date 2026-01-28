ac2efc5d25d0f712144bf55861b49929fea2286b
lonewolfyx <877085893@qq.com>
2026-01-28 15:16:22 +0800

    refactor(app): replace button component with NuxtWelcome

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,14 +1,8 @@
 <script lang="ts" setup>
-import { Button } from '@nuxt-monorepo-template/shadcn-vue/components/ui/button'
 </script>
 
 <template>
     <div>
-        <Button
-            as-child
-            variant="destructive"
-        >
-            <span class="text-3xl">hello world</span>
-        </Button>
+        <NuxtWelcome />
     </div>
 </template>
